24e00c91161a702eba21b19067396ac930a35aa6
Riyathomson <riyathomson2018@gmail.com>
2023-04-16 17:03:00 +0530

    done

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <div>
+
+
+
+
+
+  
+            </div>
+        );
+    }
+}
+
+export default Contact;
